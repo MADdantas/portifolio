8128bab0558f8ac0392b26de77b7b81d2219a0d3
Matheus <MADdantas@users.noreply.github.com>
2023-05-29 16:52:30 -0300

    testing release

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,8 +4,8 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 test('Renders without crashing', ()=>{
-    
-    let z = 200;
+
+    const z = 0;
     render(<App />);
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
