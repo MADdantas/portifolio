fb7230fbc3b496e894c648fe7af00762d14a038f
Matheus <MADdantas@users.noreply.github.com>
2023-06-01 16:02:12 -0300

    teste verdadeiro

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,7 +4,8 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 test('Renders without crashing', ()=>{
-    const test = 69;
+
+    const test = 0;
     render(<App />);
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
